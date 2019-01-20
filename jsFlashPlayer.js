e85ab3cf10b0cfb962cbb903a54938ff59e4d20f
Tomilin Dmitriy <bankastudio@gmail.com>
2019-01-20 13:55:44 +0300

    emulation of navigation arrays

--- jsFlashPlayer.js
+++ jsFlashPlayer.js
@@ -1,29 +1,23 @@
 "use strict";
 
-/*function ActiveXObject(name){
-    this.log = function (message){
-        console.log('ActiveXObject: '+message);
-    }
-    this.GetVariable=function(varname){
-        this.log('GetVariable: '+varname);
-    }
-    
-    this.log(name);
+//emulate navigation array for library sfwobject (https://github.com/swfobject/swfobject/blob/master/swfobject/src/swfobject.js)
+console.log('jsFlashPlayer start');
+var plugin = {
+    name : 'Shockwave Flash',
+    filename : '',
+    description : 'Shockwave Flash 28.0 r0',
+    version : 'Shockwave Flash 28.0 r0'
 }
-*/
-
-class ActiveXObject{
-    constructor(name){
-        console.log(name);
-    }
-    GetVariable(name){
-        console.log(name);
-    }
+window.wrappedJSObject.navigator.plugins[plugin.name]=cloneInto(plugin,window);
+var mimetype = {
+    type : 'application/x-shockwave-flash',
+    suffixes : 'swf',
+    enabledPlugin : window.wrappedJSObject.navigator.plugins[plugin.name]
 }
+window.wrappedJSObject.navigator.mimeTypes[mimetype.type]=cloneInto(mimetype,window);
 
+//searching flash objects in the DOM
 
-exportFunction(ActiveXObject,window, {defineAs:'ActiveXObject'});
-exportFunction(ActiveXObject.GetVariable, ActiveXObject, {defineAs:'GetVariable'});
 
 console.log("ok");
 
